89e2dc02df1211c94a8eae046fcf533d6d5bff60
Dzmitry Ilyin <Dzmitry_Ilyin@epam.com>
2021-05-24 17:04:00 +0300

    Exercise_3_2_solved

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,32 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger counter = 0;
+    for (int i = 0; i < ratingArray.count; i++)
+    {
+        for (int j = i + 1; j < ratingArray.count; j++)
+        {
+            if(ratingArray[i].intValue > ratingArray[j].intValue)
+            {
+                for (int k = j + 1; k < ratingArray.count; k++)
+                {
+                    if (ratingArray[j].intValue > ratingArray[k].intValue) {
+                        counter++;
+                    }
+                }
+            } else
+            {
+                for (int k = j + 1; k < ratingArray.count; k++)
+                {
+                    if (ratingArray[j].intValue < ratingArray[k].intValue) {
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
